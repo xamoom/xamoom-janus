5fc8af52f14a323ced6dbf0d5c21176306b86fa9
Bruno Hautzenberger <bruno@xamoom.com>
2018-04-04 09:45:40 +0200

    generated installable package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="janus",
+    version="1.0.1",
+    author_email="bruno@xamoom.com",
+    description="a lightweight Python framework to easily build RESTful [json:api](http://jsonapi.org/) webservices",
+    packages=['janus',]
+)
