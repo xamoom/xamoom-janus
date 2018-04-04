ef77b37ce88441c414bef6f48fa23aecbc3768d2
Bruno Hautzenberger <bruno@xamoom.com>
2018-04-04 11:15:32 +0200

    fixed versions again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="janus",
-    version="1.0.2",
+    version="1.0.3",
     author_email="bruno@xamoom.com",
     description="a lightweight Python framework to easily build RESTful [json:api](http://jsonapi.org/) webservices",
     packages=['janus',]
