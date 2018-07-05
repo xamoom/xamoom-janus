2f4849bb0d831f071b79af74645e1ad73e923708
Bruno Hautzenberger <bruno@xamoom.com>
2018-07-05 16:17:25 +0200

    1.1.5

--- janus/__init__.py
+++ janus/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Bruno Hautzenberger"
 __copyright__ = "Copyright 2018, xamoom GmbH"
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 __maintainer__ = "Bruno Hautzenberger"
 __email__ = "bruno@xamoom.com"
 __status__ = "Production"
